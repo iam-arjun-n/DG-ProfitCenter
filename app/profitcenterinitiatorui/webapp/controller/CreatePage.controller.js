e9e78b4adf2d3fa58ea6ba447011a4c74d302f00
Somshuddha Datta <somdatta@deloitte.com>
2024-07-22 06:09:40 +0000

    added xlsx files

--- app/profitcenterinitiatorui/webapp/controller/CreatePage.controller.js
+++ app/profitcenterinitiatorui/webapp/controller/CreatePage.controller.js
@@ -1,157 +1,285 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageBox",
-        "sap/ui/core/Fragment",
-        "sap/ui/model/json/JSONModel",
-        "sap/ui/core/format/DateFormat",
-        'sap/ui/export/library',
-        'sap/ui/export/Spreadsheet',
-        'sap/m/MessageToast',
-        'sap/m/StandardListItem',
-        'sap/ui/model/Filter',
-        'sap/ui/model/FilterOperator',
-        "sap/m/MessagePopover",
-        "sap/m/MessageItem",
-        "sap/m/Token",
-    ],
-    function (Controller, MessageBox, Fragment, JSONModel, DateFormat,
-        exportLibrary, Spreadsheet, MessageToast, StandardListItem,
-        Filter, FilterOperator, MessagePopover, MessageItem, Token) {
-        "use strict";
+  [
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/core/format/DateFormat",
+    "sap/ui/export/library",
+    "sap/ui/export/Spreadsheet",
+    "sap/m/MessageToast",
+    "sap/m/StandardListItem",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessagePopover",
+    "sap/m/MessageItem",
+    "sap/m/Token",
+  ],
+  function (
+    Controller,
+    MessageBox,
+    Fragment,
+    JSONModel,
+    DateFormat,
+    exportLibrary,
+    Spreadsheet,
+    MessageToast,
+    StandardListItem,
+    Filter,
+    FilterOperator,
+    MessagePopover,
+    MessageItem,
+    Token
+  ) {
+    "use strict";
 
-        return Controller.extend("mdg.profitcenter.ui.profitcenterinitiatorui.controller.CreatePage", {
-            onInit: async function () {
+    return Controller.extend(
+      "mdg.profitcenter.ui.profitcenterinitiatorui.controller.CreatePage",
+      {
+        onInit: async function () {
+          //** Passing filters to remove empty data if presents
 
-                //** Passing filters to remove empty data if presents 
+          let VHModel = this.getOwnerComponent().getModel();
+          let aFilterCC = new Filter("CompanyCode", FilterOperator.NE, " ");
 
-                let VHModel = this.getOwnerComponent().getModel();
-                let aFilterCC = new Filter("CompanyCode", FilterOperator.NE, " ");
+          //** fetching company code data from capm service & setting it to ccmodel
 
-                //** fetching company code data from capm service & setting it to ccmodel
+          let CCData = await this.oDataPromise(
+            VHModel,
+            "CompanyCodeVH",
+            aFilterCC
+          );
 
-                let CCData = await this.oDataPromise(VHModel, "CompanyCodeVH", aFilterCC);
+          let CCModel = new JSONModel();
+          CCModel.setData(CCData);
+          this.getView().setModel(CCModel, "CCModel");
 
-                let CCModel = new JSONModel();
-                CCModel.setData(CCData);
-                this.getView().setModel(CCModel, "CCModel");
+          //Download Excel Template
 
-                //Download Excel Template
+          let downloadExcelModel = new JSONModel(
+            sap.ui.require.toUrl(
+              "assetmasterinitiatorui/model/ExcelSheetTemplate.json"
+            )
+          );
+          this.getView().setModel(downloadExcelModel, "ExcelTmpModel");
+        },
 
-                let downloadExcelModel = new JSONModel(sap.ui.require.toUrl("assetmasterinitiatorui/model/ExcelSheetTemplate.json"));
-                this.getView().setModel(downloadExcelModel, "ExcelTmpModel");
+        /** Promise to fetch data */
 
+        /** to create new PC data */
+
+        addProfitCenterobj: async function () {
+          this._mode = "A"; //add
+          let oView = this.getView();
+
+          let initialisePCModel = {
+            profitcenter: "",
+            controllingarea: "",
+            name: "",
+            analysisPeriodValidFrom: "",
+            analysisPeriodValidTo: "",
+            longText: "",
+            personresponsible: "",
+            profitCentGroup: "",
+            segment: "",
+            companycode: "",
+            companyname: "",
+            assigned: false,
+            lockindicator: false,
+            enableTabs: false,
+            createdBy: '',
+            enteredOn: new Date()
+          };
+
+          //open the fragment
+
+          if (!this._PCFragment) {
+            this._PCFragment = await Fragment.load({
+              id: oView.getId(),
+              name: "mdg.profitcenter.ui.profitcenterinitiatorui.fragments.CreatePC",
+              controller: this,
+            });
+          }
+          oView.addDependent(this._PCFragment);
+
+          //initialise data model
+          oView.setModel(new JSONModel(initialisePCModel), "oPCModel");
+          this.byId("dAddProfitCenter").setTitle(`Add Profit Center`);
+
+          this._PCFragment.open();
+        },
+
+        /** to close /cancel fragment */
+
+        onCancelPCFragment() {
+          this._PCFragment.close();
+        },
+
+        /*
+         * Function - handleMessagePopoverPress
+         * Type - Event Hanlder for Message Popover button
+         * Logics:
+         *       1. Toggling between open and close state of Message Popover
+         */
+        handleMessagePopoverPressFrag: function (oEvent) {
+          this.oMessagePopoverFrag.toggle(oEvent.getSource());
+        },
+
+        /*
+         * Function - createMessageModel
+         * Type - Custom Method
+         * Logics:
+         *       1. Initializing the Message Model for Message Popover
+         */
+        createMessageModelFrag: function () {
+          //Message Template
+          let oMessageTemplate = new MessageItem({
+            type: "{messagefrag>type}",
+            title: "{messagefrag>title}",
+            activeTitle: "{messagefrag>active}",
+            description: "{messagefrag>description}",
+            subtitle: "{messagefrag>subtitle}",
+            counter: "{messagefrag>counter}",
+          });
+          /** Message Pop Over */
+          this.oMessagePopoverFrag = new MessagePopover({
+            items: {
+              path: "messagefrag>/messages",
+              template: oMessageTemplate,
             },
+          });
+          let oModel = new JSONModel();
 
-            /** Promise to fetch data */
+          this.getView().setModel(oModel, "messagefrag");
+          this.getView().getModel("messagefrag").setProperty("/messagesLength", 0);
+          this.getView().getModel("messagefrag").setProperty("/messages", []);
+          this.byId("bMsgRec").addDependent(this.oMessagePopoverFrag);
+        },
 
+        /** whnever an input changes */
 
-            /** function to trigger workflow */
+        onInputValueChange(oEvtControl) {
+          let sNewValue = oEvtControl.getParameter('newValue');
+          let oPCModel = this.getView().getModel("oPCModel");
+          if (oPCModel.getProperty("/profitcenter") !== '' && oPCModel.getProperty("/controllingarea") !== '') {
+            oPCModel.setProperty("/enableTabs", true);
+          }
+        },
 
-            triggerWorkflowFn: async function (ReqId) {
-                let commentData = this.getView().getModel("commentModel")?.getData();
-                let definitionId = `mdg.assetmaster.myassetmasterworkflow`;
-                let initialContext = {};
-                initialContext.ReqId = this.reqId;   //NEWREQUEST
-                initialContext.commentCollection = commentData;
-                initialContext.requestType = ReqId;   //REQ Number
-                let workflowPayload = {
-                    definitionId: definitionId,
-                    context: initialContext
-                }
-                let responseData = await new Promise((resolve, reject) => {
-                    $.ajax({
-                        url: this._getWorkflowRuntimeBaseURL() + "/workflow-instances",
-                        method: "POST",
-                        async: false,
-                        contentType: "application/json",
-                        headers: {
-                            "X-CSRF-Token": this._fetchToken(),
-                        },
-                        data: JSON.stringify(workflowPayload),
-                        success: function (result, xhr, data) {
-                            resolve(result);
-                            this.reqId = '';
-                        }.bind(this),
-                        error: function (request, status, error) {
-                            reject(request);
-                        },
-                    });
-                });
-                return responseData;
+        /** function to trigger workflow */
+
+        triggerWorkflowFn: async function (ReqId) {
+          let commentData = this.getView().getModel("commentModel")?.getData();
+          let definitionId = `mdg.assetmaster.myassetmasterworkflow`;
+          let initialContext = {};
+          initialContext.ReqId = this.reqId; //NEWREQUEST
+          initialContext.commentCollection = commentData;
+          initialContext.requestType = ReqId; //REQ Number
+          let workflowPayload = {
+            definitionId: definitionId,
+            context: initialContext,
+          };
+          let responseData = await new Promise((resolve, reject) => {
+            $.ajax({
+              url: this._getWorkflowRuntimeBaseURL() + "/workflow-instances",
+              method: "POST",
+              async: false,
+              contentType: "application/json",
+              headers: {
+                "X-CSRF-Token": this._fetchToken(),
+              },
+              data: JSON.stringify(workflowPayload),
+              success: function (result, xhr, data) {
+                resolve(result);
+                this.reqId = "";
+              }.bind(this),
+              error: function (request, status, error) {
+                reject(request);
+              },
+            });
+          });
+          return responseData;
+        },
+        _getWorkflowRuntimeBaseURL: function () {
+          var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
+          var appPath = appId.replaceAll(".", "/");
+          var appModulePath = jQuery.sap.getModulePath(appPath);
+
+          return appModulePath + "/bpmworkflowruntime/v1";
+        },
+        _fetchToken: function (rules) {
+          let fetchedToken, url;
+
+          url = this._getWorkflowRuntimeBaseURL() + "/xsrf-token";
+
+          jQuery.ajax({
+            url: url,
+            method: "GET",
+            async: false,
+            headers: {
+              "X-CSRF-Token": "Fetch",
             },
-            _getWorkflowRuntimeBaseURL: function () {
-                var appId = this.getOwnerComponent().getManifestEntry("/sap.app/id");
-                var appPath = appId.replaceAll(".", "/");
-                var appModulePath = jQuery.sap.getModulePath(appPath);
-
-                return appModulePath + "/bpmworkflowruntime/v1";
+            success(result, xhr, data) {
+              fetchedToken = data.getResponseHeader("X-CSRF-Token");
             },
-            _fetchToken: function (rules) {
-                let fetchedToken, url;
+          });
+          return fetchedToken;
+        },
 
-                url = this._getWorkflowRuntimeBaseURL() + "/xsrf-token";
+        /** Feed Input comment collection function */
 
-                jQuery.ajax({
-                    url: url,
-                    method: "GET",
-                    async: false,
-                    headers: {
-                        "X-CSRF-Token": "Fetch",
-                    },
-                    success(result, xhr, data) {
-                        fetchedToken = data.getResponseHeader("X-CSRF-Token");
-                    },
-                });
-                return fetchedToken;
-            },
+        onPostComment: function () {
+          var oFormat = DateFormat.getDateTimeInstance({ style: "medium" });
+          var oComments = this.getView().getModel("commentModel");
+          var oDate = new Date();
+          var sDate = oFormat.format(oDate);
+          // create new entry
+          var sValue = oEvent.getParameter("value");
+          var aComments = oComments.getData();
+          var oEntry = {
+            // UserName: this.getOwnerComponent().currentUser,
+            UserName: "test@user.com",
+            Date: "" + sDate,
+            Text: sValue,
+          };
+          aComments.unshift(oEntry);
+          oComments.setData(aComments);
+        },
 
-            /** Feed Input comment collection function */
+        /** download excel template function */
 
-            onPostComment: function () {
-                var oFormat = DateFormat.getDateTimeInstance({ style: "medium" });
-                var oComments = this.getView().getModel("commentModel");
-                var oDate = new Date();
-                var sDate = oFormat.format(oDate);
-                // create new entry
-                var sValue = oEvent.getParameter("value");
-                var aComments = oComments.getData();
-                var oEntry = {
-                    // UserName: this.getOwnerComponent().currentUser,
-                    UserName: "test@user.com",
-                    Date: "" + sDate,
-                    Text: sValue
-                };
-                aComments.unshift(oEntry);
-                oComments.setData(aComments);
-            },
+        onPressDownloadTemplate: function () {
+          let excelWorkBookData = this.getView()
+            .getModel("ExcelTmpModel")
+            .getData();
+          const Workbook = XLSX.utils.book_new();
+          excelWorkBookData.forEach((sheet) => {
+            if (sheet.data.length > 0) {
+              const sheetData = XLSX.utils.aoa_to_sheet([
+                Object.keys(sheet.data[0]),
+                ...sheet.data.map(Object.values),
+              ]);
+              XLSX.utils.book_append_sheet(Workbook, sheetData, sheet.Name);
+            }
+          });
 
-            /** download excel template function */
+          // Convert the workbook to binary format
+          const excelBinary = XLSX.write(Workbook, { type: "binary" });
 
-            onPressDownloadTemplate: function () {
-                let excelWorkBookData = this.getView().getModel("ExcelTmpModel").getData();
-                const Workbook = XLSX.utils.book_new();
-                excelWorkBookData.forEach(sheet => {
-                    if (sheet.data.length > 0) {
-                        const sheetData = XLSX.utils.aoa_to_sheet([Object.keys(sheet.data[0]), ...sheet.data.map(Object.values)]);
-                        XLSX.utils.book_append_sheet(Workbook, sheetData, sheet.Name);
-                    }
-                });
+          // Trigger the download
+          const blob = new Blob([this.s2ab(excelBinary)], {
+            type: "application/octet-stream",
+          });
+          saveAs(blob, "assetmaster_template.xlsx"); // <-- Use saveAs from FileSaver.js
+        },
 
-                // Convert the workbook to binary format
-                const excelBinary = XLSX.write(Workbook, { type: "binary" });
-
-                // Trigger the download
-                const blob = new Blob([this.s2ab(excelBinary)], { type: "application/octet-stream" });
-                saveAs(blob, "assetmaster_template.xlsx"); // <-- Use saveAs from FileSaver.js 
-            },
-
-            s2ab: function (s) {
-                const buf = new ArrayBuffer(s.length);
-                const view = new Uint8Array(buf);
-                for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
-                return buf;
-            },
-        });
-    }
+        s2ab: function (s) {
+          const buf = new ArrayBuffer(s.length);
+          const view = new Uint8Array(buf);
+          for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xff;
+          return buf;
+        },
+      }
+    );
+  }
 );
